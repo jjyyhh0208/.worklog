e169bfa61c22cd3f869bcd715b19b087c701071e
jhkimon <sky06098i1@gmail.com>
2024-07-08 00:31:50 +0900

    urls.py 패턴에 맞게 수정

--- backend/worklog/profiles/urls.py
+++ backend/worklog/profiles/urls.py
@@ -8,10 +8,10 @@ router.register(r'interests', InterestViewSet, basename='interest')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('view/', UserProfileView.as_view(), name='user-profile-view'),
-    path('user_work_interest_set/', UserWorkInterestView.as_view(), name='user-work-interest-update'),  # 유저의 업무 성향, 관심 직종 설정 엔드포인트
-    path('user_info_set/', UserGenderNameAgeView.as_view(), name='user-info-update'),  # 유저의 이름, 성별, 나이 설정 엔드포인트
+    path('user/view/', UserProfileView.as_view(), name='user-profile-view'),
+    path('user/set/work-interest/', UserWorkInterestView.as_view(), name='user-work-interest-update'),  # 유저의 업무 성향, 관심 직종 설정 엔드포인트
+    path('user/set/basic-info/', UserGenderNameAgeView.as_view(), name='user-info-update'),  # 유저의 이름, 성별, 나이 설정 엔드포인트
     path('auth/', include('dj_rest_auth.urls')),  # 로그인, 로그아웃, 비밀번호 변경, 비밀번호 초기화 등을 위한 엔드포인트 기본 제공 기능
     path('auth/registration/', include('dj_rest_auth.registration.urls')),  # 회원가입을 위한 엔드포인트 기본 제공 기능
-    path('auth/check-username/', UsernameUniqueCheck.as_view(), name='check-username'), # 유저 중복 확인
+    path('auth/check-username/', UsernameUniqueCheck.as_view(), name='check-username'), # 유저 이름 중복 검사
 ]
