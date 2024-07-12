f9d06cf3c052d74e24fa69c99e66e56e597a5e4c
sososoyun <arl700@naver.com>
2024-07-13 06:59:09 +0900

    myprofile ui 80%

---
+++ frontend/src/pages/MyProfile/MyProfile11.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+import AdminService from '../../utils/AdminService';
+import ProfileService from '../../utils/ProfileService';
+import styles from './MyProfile.module.css';
+
+function MyProfile() {
+    const [profileData, setProfileData] = useState(null);
+
+    useEffect(() => {
+        ProfileService.fetchUserProfile()
+            .then((data) => {
+                setProfileData(data);
+            })
+            .catch((error) => {
+                console.error('프로필 정보를 불러오는 동안 오류가 발생했습니다.', error);
+            });
+    }, []);
+
+    return (
+        <div>
+            <h2>단순 버전 - 내 프로필 페이지</h2>
+            {profileData ? (
+                <div>
+                    <p>이름: {profileData.name}</p>
+                    <p>성별: {profileData.gender}</p>
+                    <p>나이: {profileData.age}</p>
+                    <p>업무 스타일:</p>
+                    <ul>
+                        {profileData.work_styles.map((style) => (
+                            <li key={style.id}>{style.name}</li>
+                        ))}
+                    </ul>
+                    <p>관심사:</p>
+                    <ul>
+                        {profileData.interests.map((interest) => (
+                            <li key={interest.id}>{interest.name}</li>
+                        ))}
+                    </ul>
+                </div>
+            ) : (
+                <p>프로필 정보를 불러오는 중입니다...</p>
+            )}
+        </div>
+    );
+}
+
+export default MyProfile;
