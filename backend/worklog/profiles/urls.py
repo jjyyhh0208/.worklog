864cd963d9b84d767315741fb4015168df38ca1f
jhkimon <sky06098i1@gmail.com>
2024-07-15 21:00:41 +0900

    엔드포인트 변경

--- backend/worklog/profiles/urls.py
+++ backend/worklog/profiles/urls.py
@@ -15,20 +15,23 @@ router.register(r'workstyles', WorkStyleViewSet, basename='workstyle')
 router.register(r'interests', InterestViewSet, basename='interest')
 router.register(r'short-questions', ShortQuestionViewSet, basename='short-question')
 router.register(r'long-questions', LongQuestionViewSet)
-router.register(r'feedbacks', FeedbackViewSet)
+router.register(r'user/set/feedbacks', FeedbackViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    
+    # User View
     path('user/view/current/', UserCurrentProfileView.as_view(), name='user-current-profile-view'),
     path('user/view/<str:username>/', UserProfileView.as_view(), name='user-profile-view'), # username 별로 user 값을 불러올 수 있는 엔드포인트
     path('user/view/long-question/<str:username>/', UserLongQuestionView.as_view(), name='longquestions-by-user'), # 유저에 맞는 질문을 반환하는 엔드포인트
     path('user/view/feedback/<str:username>/', FeedbackByUserView.as_view(), name='feedback-by-user'), # 유저에 맞는 피드백을 반환하는 엔드포인트
     path('user/view/friends/<str:username>/', UserFriendView.as_view(), name='user-friend-view'), # 유저의 친구 목록을 보여주는 엔드포인트
-
-    path('user/set/work-style/', UserWorkStyleView.as_view(), name='user-work-style-update'),  # 유저의 업무 성향, 관심 직종 설정 엔드포인트
-    path('user/set/interest/', UserInterestView.as_view(), name='user-work-interest-update'),  # 유저의 업무 성향, 관심 직종 설정 엔드포인트
+    
+    # User Setting
+    path('user/set/work-style/', UserWorkStyleView.as_view(), name='user-work-style-update'),  # 유저의 업무 성향 직종 설정 엔드포인트
+    path('user/set/interest/', UserInterestView.as_view(), name='user-work-interest-update'),  # 유저의 관심 직종 설정 엔드포인트
     path('user/set/basic-info/', UserGenderNameAgeView.as_view(), name='user-info-update'),  # 유저의 이름, 성별, 나이 설정 엔드포인트
+
+    # Auth
     path('auth/', include('dj_rest_auth.urls')),  # 로그인, 로그아웃, 비밀번호 변경, 비밀번호 초기화 등을 위한 엔드포인트 기본 제공 기능
     path('auth/registration/', include('dj_rest_auth.registration.urls')),  # 회원가입을 위한 엔드포인트 기본 제공 기능
     path('auth/check-username/', UniqueIdCheck.as_view(), name='check-username'), # 유저 이름 중복 검사
