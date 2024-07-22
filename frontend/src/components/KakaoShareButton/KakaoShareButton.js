ac1a81d0351246d3a194349d026437e64967ad8b
kimgrit <sungmini1102@gmail.com>
2024-07-23 03:18:04 +0900

    fix: kakaoshare

--- frontend/src/components/KakaoShareButton/KakaoShareButton.js
+++ frontend/src/components/KakaoShareButton/KakaoShareButton.js
@@ -1,24 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import ProfileService from '../../utils/ProfileService';
 
-const KakaoShareButton = () => {
+const KakaoShareButton = ({ username }) => {
+    const [profileData, setProfileData] = useState(null);
+
+    useEffect(() => {
+        const fetchProfileData = async () => {
+            try {
+                const data = await ProfileService.fetchFriendProfile(username);
+                setProfileData(data);
+            } catch (error) {
+                console.error('Error fetching profile data:', error);
+            }
+        };
+
+        fetchProfileData();
+    }, [username]);
+
     const shareToKakao = () => {
+        if (!profileData) return;
+
         window.Kakao.Link.sendDefault({
             objectType: 'feed',
             content: {
-                title: '.WORKLOG: 일하는 모두를 위한 서비스',
-                description: '다른 사람은 나를 어떻게 보고 있을까요? 지금 업무성향을 체크하러 가봐요!',
+                title: '.WORKLOG - 익명 팀원 피드백 서비스',
+                description: `${profileData.name}님이 더 발전할 수 있도록, 협업 피드백을 남겨주세요. 팀원의 솔직한 피드백은 서로를 성장 시킵니다.`,
                 imageUrl: 'https://dot-worklog.com/images/logo.png',
                 link: {
-                    mobileWebUrl: 'https://dot-worklog',
-                    webUrl: 'https://dot-worklog',
+                    mobileWebUrl: `https://dot-worklog.com/friend-profile/${username}`,
+                    webUrl: `https://dot-worklog.com/friend-profile/${username}`,
                 },
             },
             buttons: [
                 {
                     title: '웹으로 보기',
                     link: {
-                        mobileWebUrl: 'https://dot-worklog',
-                        webUrl: 'https://dot-worklog',
+                        mobileWebUrl: `https://dot-worklog.com/friend-profile/${username}`,
+                        webUrl: `https://dot-worklog.com/friend-profile/${username}`,
                     },
                 },
             ],
